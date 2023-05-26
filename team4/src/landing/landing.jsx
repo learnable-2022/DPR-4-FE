5cfd8886485243ddcd7b4b7f70a6cb3cc3cff834
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-05-26 17:48:31 +0100

    added nav-section

--- team4/src/landing/landing.jsx
+++ team4/src/landing/landing.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import female from '../landing/Images/femaleDoc.png';
-import logo from '../landing/Images/logo.png';
+import femaleDoc from '../landing/images/femaleDoc.png';
+import logo from '../landing/images/logo.png';
+import male from '../landing/images/maleDoc.png';
+import heart from '../landing/images/heart.png';
+import pill from '../landing/images/pill.png';
+import '../landing/landing.css'
+import { Link } from 'react-router-dom';
 
 
 export default function Landing() {
@@ -9,19 +14,36 @@ export default function Landing() {
 
       {/* nav section */}
       <div className="nav-section">
-      <div className="">
-        <img src={logo} alt="perfume2" id="logo"/>
-        <img src={female} alt="perfume2" id="female-img"/>
+
+        <div className="nav1">
+          <img src={logo} alt="perfume2" id="logo"/>
+        </div>
+
+      <div className="nav2">
+        <Link className='link'  to=""><li>Home</li></Link>
+        <Link className='link'  to=""><li>Features</li></Link>
+        <Link className='link'  to=""><li>Pricing</li></Link>
+        <Link className='link'  to=""><li>Support</li></Link>
+        <Link className='link'  to=""><button>Get Started</button></Link>
       </div>
 
-      <div className="">
-      <Link className='navi-link'  to="vaccine">Vaccines</Link>
       </div>
-      </div>
+
+      
 
        {/* hero section */}
       <div className="hero-section">
-        
+
+        <div className="hero-1">
+          <h1>Simplify Medical Record Keeping with <span>Medbloc</span></h1>
+          <p>Keep all your patient data organized & secure in one easy-to-use desktop application.</p>
+          <Link className='link'  to=""><button>join Now</button></Link>
+        </div>
+
+        <div className="hero-2">
+          <img src={femaleDoc} alt="perfume2" id="female-img"/>
+        </div>
+
       </div>
 
       {/* section-one */}
