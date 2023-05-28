65b3b046e1263c14af26261f286b5b3477d655cc
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-05-28 02:33:32 +0100

    added section 1,2,3,4 and footer

--- team4/src/Record/PatientsRecord.jsx
+++ team4/src/Record/PatientsRecord.jsx
@@ -1,88 +1,88 @@
-import React from 'react';
-import {IoIosNotificationsOutline} from "react-icons/io";
-import {BsArrowLeft} from "react-icons/bs";
-<<<<<<< HEAD
-import logo2 from "../images/Rectangle 20.png";
-import pic1 from "../images/Rectangle 10.png";
-=======
-import logo2 from "../assets/Rectangle 20.png";
-import pic1 from "../assets/Rectangle 10.png";
->>>>>>> 9570247e689880d173c744e45156af3d5d3763b7
-import './Record.css';
-import { Link, Outlet } from 'react-router-dom';
+// import React from 'react';
+// import {IoIosNotificationsOutline} from "react-icons/io";
+// import {BsArrowLeft} from "react-icons/bs";
+// <<<<<<< HEAD
+// import logo2 from "../images/Rectangle 20.png";
+// import pic1 from "../images/Rectangle 10.png";
+// =======
+// import logo2 from "../assets/Rectangle 20.png";
+// import pic1 from "../assets/Rectangle 10.png";
+// >>>>>>> 9570247e689880d173c744e45156af3d5d3763b7
+// import './Record.css';
+// import { Link, Outlet } from 'react-router-dom';
 
-export default function PatientsRecord() {
-  return (
-    <div className='container'>
-        <nav className='nav-container'>
-          <div className='nav-container-left'>
-            <BsArrowLeft/>
-            <h2>
-            Medical Record
-            </h2>
-          </div>
-          <div className='nav-container-right'>
-            <div>
-            <button>
-            share report
-            </button>
-            </div>
-            <div>
-              <IoIosNotificationsOutline/>
-            </div>
-            <div className='nav-short'>
-              <img src={logo2}alt="frame"/>
-              <p>
-              Mr John Doe
-              </p>
-            </div>
-          </div>
-        </nav>
-      <div className='second-section'>
-        <div className='second-section-image'>
-        <img src={pic1} alt= "profile-pic"/>
-        </div>
-        <div className='second-section-text'>
-          <div>
-            <h2>Name</h2>
-            <p>John</p>
-            <h2>Gender</h2>
-            <p>M</p>
-            <h2>Height</h2>
-            <p>5ft 8inc</p>
-          </div>
-          <div>
-            <h2>Middle Name</h2>
-            <p>-</p>
-            <h2>Genotype</h2>
-            <p>AA</p>
-            <h2>Weight</h2>
-            <p>62kg</p>
-          </div>
-          <div>
-            <h2>Surname</h2>
-            <p>Doe</p>
-            <h2>Blood Group</h2>
-            <p>O+</p>
-            <h2>Allergies</h2>
-            <p>Peanut, Aspirin, Penicillin</p>
-          </div>
-          <div>
-            <h2>Medical Condition</h2>
-            <p>Asthma</p>
-          </div>
-        </div>
-      </div>
-      <div className='third-section-link'>
-        <Link  className='navi-link'  to="overview"><p>Overview</p></Link>
-        <Link  className='navi-link' to="visit"><p>Visits</p></Link>
-        <Link  className='navi-link' to="lab"><p>Labs</p></Link>
-        <Link className='navi-link'  to="vaccine"><p>Vaccines</p></Link>
-        <Link  className='navi-link' to="prescription"><p>Prescription</p></Link>
-      </div>
-      <div>
-      <Outlet/>
-      </div>
-    </div>
-  )
-}
+// export default function PatientsRecord() {
+//   return (
+//     <div className='container'>
+//         <nav className='nav-container'>
+//           <div className='nav-container-left'>
+//             <BsArrowLeft/>
+//             <h2>
+//             Medical Record
+//             </h2>
+//           </div>
+//           <div className='nav-container-right'>
+//             <div>
+//             <button>
+//             share report
+//             </button>
+//             </div>
+//             <div>
+//               <IoIosNotificationsOutline/>
+//             </div>
+//             <div className='nav-short'>
+//               <img src={logo2}alt="frame"/>
+//               <p>
+//               Mr John Doe
+//               </p>
+//             </div>
+//           </div>
+//         </nav>
+//       <div className='second-section'>
+//         <div className='second-section-image'>
+//         <img src={pic1} alt= "profile-pic"/>
+//         </div>
+//         <div className='second-section-text'>
+//           <div>
+//             <h2>Name</h2>
+//             <p>John</p>
+//             <h2>Gender</h2>
+//             <p>M</p>
+//             <h2>Height</h2>
+//             <p>5ft 8inc</p>
+//           </div>
+//           <div>
+//             <h2>Middle Name</h2>
+//             <p>-</p>
+//             <h2>Genotype</h2>
+//             <p>AA</p>
+//             <h2>Weight</h2>
+//             <p>62kg</p>
+//           </div>
+//           <div>
+//             <h2>Surname</h2>
+//             <p>Doe</p>
+//             <h2>Blood Group</h2>
+//             <p>O+</p>
+//             <h2>Allergies</h2>
+//             <p>Peanut, Aspirin, Penicillin</p>
+//           </div>
+//           <div>
+//             <h2>Medical Condition</h2>
+//             <p>Asthma</p>
+//           </div>
+//         </div>
+//       </div>
+//       <div className='third-section-link'>
+//         <Link  className='navi-link'  to="overview"><p>Overview</p></Link>
+//         <Link  className='navi-link' to="visit"><p>Visits</p></Link>
+//         <Link  className='navi-link' to="lab"><p>Labs</p></Link>
+//         <Link className='navi-link'  to="vaccine"><p>Vaccines</p></Link>
+//         <Link  className='navi-link' to="prescription"><p>Prescription</p></Link>
+//       </div>
+//       <div>
+//       <Outlet/>
+//       </div>
+//     </div>
+//   )
+// }
--- team4/src/landing/landing.jsx
+++ team4/src/landing/landing.jsx
@@ -12,7 +12,7 @@ export default function Landing() {
   return (
     <div className="">
 
-      {/* nav section */}
+{/* nav section */}
       <div className="nav-section">
 
         <div className="nav1">
@@ -31,7 +31,7 @@ export default function Landing() {
 
       
 
-       {/* hero section */}
+{/* hero section */}
       <div className="hero-section">
 
         <div className="hero-1">
@@ -46,7 +46,7 @@ export default function Landing() {
 
       </div>
 
-      {/* section-one */}
+{/* section-one */}
       <div className="section-one">
         <div>
           <img src={pill} alt="pill" id="pill-img"/>
@@ -62,24 +62,103 @@ export default function Landing() {
         </div>
       </div>
 
-       {/* section-two*/}
+{/* section-two*/}
       <div className="section-two">
-
+        <div className="section-two-a">
+          <img src={male} alt="male-doctor" id="male-img"/>
+        </div>
+        <div className="section-two-b">
+          <h2 className="section-two-text">We Constantly Provide the Greatest Medical Care Record for your Health.</h2>
+          <ul className="list">
+            <li className="section-2-list">
+              <i className="ri-checkbox-line"></i>
+              <p>State of the Art Doctor Services</p>
+            </li>
+            <li className="section-2-list">
+              <i className="ri-checkbox-line"></i>
+              <p>Enrollment is Quick and Easy</p>
+            </li>
+            <li className="section-2-list">
+              <i className="ri-checkbox-line"></i>
+              <p>Access to Top Specialist Doctors</p>
+            </li>
+            <li className="section-2-list">
+              <i className="ri-checkbox-line"></i>
+              <p>Low Gas Fee</p>
+            </li>
+            <Link className='link'  to=""><button className="hero-btn">Join now</button></Link>
+          </ul>
+        </div>
       </div>
 
-       {/* section-three */}
+{/* section-three */}
       <div className="section-three">
-
+        <div className="section-three-a">
+          <h2 className="section-three-text">Various Health Departments You Can Access for Comprehensive Care</h2>
+          <p className="section-three-text-b">
+          Our mission is to provide high-quality healthcare that addresses all aspects of our patients' health. We believe in treating the whole person, not just their symptoms or conditions, and empowering our patients to take an active role in their own health. With a focus on prevention and ongoing support, we aim to help our patients achieve optimal health and wellbeing
+          </p>
+          <Link className='link'  to=""><button className="hero-btn">Join now</button></Link>
+        </div>
+        <div className="section-three-b">
+          <img src={heart} alt="heart" id="heart-img"/>
+        </div>
       </div>
        {/* section-four */}
       <div className="section-four">
-
+        <div className="section-four-a">
+          <h4>Access Health Services and Tips for a Healthier Lifestyle Right Here</h4>
+          <p>
+            Take charge of your health with our comprehensive health services and expert tips for healthy living. Explore now and start your journey towards a healthier you.
+          </p>
+        </div>
+        <div className="section-four-b">
+        <Link className='link'  to=""><button className="section-four-btn">Stay connected</button></Link>
+        </div>
       </div>
 
-       {/* section-five */}
-      <div className="section-five">
-
+{/* footer-section */}
+      <div className="footer-section">
+        <div className="footer-section-a">
+          <img src={logo} alt="medbloc-logo" id="logo"/>
+          <p>
+          Have a safe and decentralized medical record with us and access expert medical opinion from one of our world renowned specialist so you can have the answer and confidence to make informed decisions about your health
+          </p>
+        </div>
+        <div className="footer-section-b">
+          <h5>Information</h5>
+          <Link className='footer-link'  to=""><p>Sign Up</p></Link>
+          <Link className='footer-link'  to=""><p>Join Community</p></Link>
+          <Link className='footer-link'  to=""><p>Learning</p></Link>
+          <Link className='footer-link'  to=""><p>Newsletter</p></Link>
+        </div>
+        <div className="footer-section-c">
+          <h5>Platform</h5>
+          <Link className='footer-link'  to=""><p>Terms of Use</p></Link>
+          <Link className='footer-link'  to=""><p>About</p></Link>
+          <Link className='footer-link'  to=""><p>Contact</p></Link>
+          <Link className='footer-link'  to=""><p>Partnership</p></Link>
+        </div>
+        <div className="footer-section-d">
+          <h5>Support</h5>
+          <Link className='footer-link'  to=""><p>Help Centre</p></Link>
+          <Link className='footer-link'  to=""><p>Video Tutorial</p></Link>
+          <Link className='footer-link'  to=""><p>Cookie Settings</p></Link>
+        </div>
       </div>
     </div>
   )
 }
+
+// texts 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// Have a safe and decentralized medical record with us and access expert medical opinion from one of our world renowned specialist so you can have the answer and confidence to make informed decisions about your health
+// Information
