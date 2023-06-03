e9e49a7d29fc6178f5d56b0b07a6650cac30be4a
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-06-03 19:10:19 +0100

    added jsx files

---
+++ team4/src/Password/newPassword.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../Password/password.css";
+import "../Password/forgotpassword.jsx";
+import logo from "../Password/images/logo";
+import desk from "../Password/images/desk";
+// import { Link } from "react-router-dom";
+
+export default function Landing() {
+    return (
+        <div className="">
+            <div className="container">
+                <div className="container-1">
+                    <img src={logo} alt="medbloc-logo" id="logo" />
+                    <h2>New Password</h2>
+                    <p>
+                        Please enter your new password
+                    </p>
+                    <input type="email" name="Email" id="email" placeholder="Email Address" required></input>
+                    <input type="submit" id="btn" value="Continue"></input>
+                </div>
+
+                <div className="container-2">
+                <img src={desk} alt="deskimage" id="desk" />
+                </div>
+            </div>
+        </div>
+    )
+}
