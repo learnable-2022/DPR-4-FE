846a861886c19187fe0f0852654098c7ab6b7e3e
chiemezie nduaguba <anthonynduaguba123@gmail.com>
2023-06-22 02:59:08 +0100

    retified changes

--- src/dashboard/PatientDashboard.jsx
+++ src/dashboard/PatientDashboard.jsx
@@ -35,7 +35,7 @@ import abi from "../abi.json";
 import { ethers } from "ethers";
 // for nav bar
 
-import { Link, useNavigate } from "react-router-dom";
+
 import { RxDashboard } from "react-icons/rx";
 import { BsReverseLayoutTextSidebarReverse } from "react-icons/bs";
 import { TfiWrite } from "react-icons/tfi";
